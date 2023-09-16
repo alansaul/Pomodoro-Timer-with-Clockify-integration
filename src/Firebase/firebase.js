10e1721b59f164b68f913f54e080bc8b90ad94a2
Alan Saul <alan.daniel.saul@gmail.com>
2023-09-16 19:01:02 +0100

    refactor: move all config of app to .env file

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -3,18 +3,22 @@ import {
 } from "firebase/app";
 import 'firebase/firestore'
 import 'firebase/auth'
+//import { getAnalytics } from "firebase/analytics";
 
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "pomodoro-timer-clockify.firebaseapp.com",
-    projectId: "pomodoro-timer-clockify",
-    storageBucket: "pomodoro-timer-clockify.appspot.com",
-    messagingSenderId: "98342232404",
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 const firebase = initializeApp(config);
+//const analytics = getAnalytics(firebase);
 
 export {
     firebase
 }
+
